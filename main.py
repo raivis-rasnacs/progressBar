fd12529b1f84819291d0701516492a31b8e2f5f7
SkolotajsRaivis <71266644+raivis-rasnacs@users.noreply.github.com>
2021-12-24 08:52:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from time import sleep
+
+progress = 0
+while progress != 100:
+  progress += 10
+  for x in range(int(progress/10)):
+    print("█", end="")
+  print(progress, "%")
+  sleep(0.5)
+  os.system("clear")
